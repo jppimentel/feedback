849657f4a859db6d7e77d7d6987b84d676515aff
unknown <joao.pimentel@engenharia.ufjf.br>
2023-06-06 23:12:31 -0300

    add fastify cors

--- server/src/server.ts
+++ server/src/server.ts
@@ -3,21 +3,29 @@ import { authRoutes } from './routes/auth';
 import { userRoutes } from './routes/user';
 import { friendsRoutes } from './routes/friends';
 import { feedbackRoutes } from './routes/feedback';
+import fastifyCors from '@fastify/cors';
 
 async function bootstrap() {
-   const fastify = Fastify({
-        logger: true, 
-   });
+  const fastify = Fastify({
+    logger: true,
+  });
 
-   await fastify.register(authRoutes);
-   await fastify.register(userRoutes);
-   await fastify.register(friendsRoutes);
-   await fastify.register(feedbackRoutes);
-    
-    await fastify.listen({
-     // port: process.env.PORT ? Number(process.env.PORT) : 3333,
-     port: 3333, 
-     host: '0.0.0.0'});
-};
+  // Registrar o plugin @fastify/cors
+  fastify.register(fastifyCors, {
+    origin: '*',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+  });
+
+  await fastify.register(authRoutes);
+  await fastify.register(userRoutes);
+  await fastify.register(friendsRoutes);
+  await fastify.register(feedbackRoutes);
+
+  await fastify.listen({
+    port: 3333,
+    host: '0.0.0.0',
+  });
+}
 
 bootstrap();
